3bea5c00c75122004038bc66f8fe7bf66898ec3e
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-06-19 10:23:12 +1200

    Adding a SignalR Hub

--- ActivityBud/API/Extensions/ApplicationServiceExtensions.cs
+++ ActivityBud/API/Extensions/ApplicationServiceExtensions.cs
@@ -13,33 +13,34 @@ using Persistence;
 
 namespace API.Extensions
 {
-    public static class ApplicationServiceExtensions
-    {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services, 
-            IConfiguration config)
-        {
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt => 
-            {
-                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt => 
-            {
-                opt.AddPolicy("CorsPolicy", policy => 
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins(new string[] {"http://localhost:3000", "test"});
-                });
-            });
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
-            services.AddScoped<IUserAccessor, UserAccessor>();
-            services.AddScoped<IPhotoAccessor, PhotoAccessor>();
-            services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
+ public static class ApplicationServiceExtensions
+ {
+  public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+      IConfiguration config)
+  {
+   services.AddSwaggerGen(c =>
+   {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+   });
+   services.AddDbContext<DataContext>(opt =>
+   {
+    opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+   });
+   services.AddCors(opt =>
+   {
+    opt.AddPolicy("CorsPolicy", policy =>
+             {
+              policy.AllowAnyMethod().AllowAnyHeader().WithOrigins(new string[] { "http://localhost:3000", "test" });
+             });
+   });
+   services.AddMediatR(typeof(List.Handler).Assembly);
+   services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+   services.AddScoped<IUserAccessor, UserAccessor>();
+   services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+   services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
+   services.AddSignalR();
 
-            return services;
-        }
-    }
+   return services;
+  }
+ }
 }
