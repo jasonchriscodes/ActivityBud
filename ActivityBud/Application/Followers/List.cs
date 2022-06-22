151ceb5de96010a2850b6799bb2260ce9229a04a
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-06-22 13:52:41 +1200

    Adding the following to mapping configuration

--- ActivityBud/Application/Followers/List.cs
+++ ActivityBud/Application/Followers/List.cs
@@ -24,8 +24,10 @@ namespace Application.Followers
   {
    private readonly DataContext context;
    private readonly IMapper mapper;
-   public Handler(DataContext context, IMapper mapper)
+   private readonly IUserAccessor userAccessor;
+   public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
    {
+    this.userAccessor = userAccessor;
     this.mapper = mapper;
     this.context = context;
    }
@@ -39,7 +41,8 @@ namespace Application.Followers
      case "followers":
       profiles = await this.context.UserFollowings.Where(x => x.Target.UserName == request.Username)
       .Select(u => u.Observer)
-      .ProjectTo<Profiles.Profile>(this.mapper.ConfigurationProvider)
+      .ProjectTo<Profiles.Profile>(this.mapper.ConfigurationProvider,
+      new { currentUsername = this.userAccessor.GetUsername()})
       .ToListAsync();
       break;
      case "following":
