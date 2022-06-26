078318d05eba912df5a6a1762f7ba0a018219a25
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-06-26 14:12:30 +1200

    Adding a user profile activities component

--- ActivityBud/client-app/src/app/stores/profileStore.ts
+++ ActivityBud/client-app/src/app/stores/profileStore.ts
@@ -1,3 +1,4 @@
+import { UserActivity } from "./../models/profile";
 import { makeAutoObservable, reaction, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Photo, Profile } from "../models/profile";
@@ -11,6 +12,8 @@ export default class ProfileStore {
   followings: Profile[] = [];
   loadingFollowings = false;
   activeTab = 0;
+  userActivities: UserActivity[] = [];
+  loadingActivities = false;
 
   constructor() {
     makeAutoObservable(this);
@@ -191,6 +194,25 @@ export default class ProfileStore {
     } catch (error) {
       console.log(error);
       runInAction(() => (this.loadingFollowings = false));
+    }
+  };
+
+  loadUserActivities = async (username: string, predicate?: string) => {
+    this.loadingActivities = true;
+    try {
+      const activities = await agent.Profiles.listActivities(
+        username,
+        predicate!
+      );
+      runInAction(() => {
+        this.userActivities = activities;
+        this.loadingActivities = false;
+      });
+    } catch (error) {
+      console.log(error);
+      runInAction(() => {
+        this.loadingActivities = false;
+      });
     }
   };
 }
