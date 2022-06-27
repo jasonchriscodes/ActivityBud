52e7dc154854db8da530f3299c9fdbe9704a4fdb
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-06-27 13:24:33 +1200

    Adding private routes

--- ActivityBud/client-app/src/app/layout/App.tsx
+++ ActivityBud/client-app/src/app/layout/App.tsx
@@ -1,21 +1,21 @@
-import React, { useEffect } from "react";
-import { Container } from "semantic-ui-react";
-import NavBar from "./NavBar";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
-import HomePage from "../../features/home/HomePage";
-import ActivityForm from "../../features/activities/form/ActivityForm";
-import ActivityDetails from "../../features/activities/details/ActivityDetails";
-import TestErrors from "../../features/errors/TestError";
 import { ToastContainer } from "react-toastify";
+import { Container } from "semantic-ui-react";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import ActivityForm from "../../features/activities/form/ActivityForm";
 import NotFound from "../../features/errors/NotFound";
 import ServerError from "../../features/errors/ServerError";
-import LoginForm from "../../features/users/LoginForm";
+import TestErrors from "../../features/errors/TestError";
+import HomePage from "../../features/home/HomePage";
+import ProfilePage from "../../features/profiles/ProfilePage";
+import ModalContainer from "../common/modals/ModalContainer";
 import { useStore } from "../stores/store";
 import LoadingComponent from "./LoadingComponent";
-import ModalContainer from "../common/modals/ModalContainer";
-import ProfilePage from "../../features/profiles/ProfilePage";
+import NavBar from "./NavBar";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
   const location = useLocation();
@@ -44,17 +44,26 @@ function App() {
             <NavBar />
             <Container style={{ marginTop: "7em" }}>
               <Switch>
-                <Route exact path="/activities" component={ActivityDashboard} />
-                <Route path="/activities/:id" component={ActivityDetails} />
-                <Route
+                <PrivateRoute
+                  exact
+                  path="/activities"
+                  component={ActivityDashboard}
+                />
+                <PrivateRoute
+                  path="/activities/:id"
+                  component={ActivityDetails}
+                />
+                <PrivateRoute
                   key={location.key}
                   path={["/createActivity", "/manage/:id"]}
                   component={ActivityForm}
                 />
-                <Route path="/profiles/:username" component={ProfilePage} />
-                <Route path="/errors" component={TestErrors} />
+                <PrivateRoute
+                  path="/profiles/:username"
+                  component={ProfilePage}
+                />
+                <PrivateRoute path="/errors" component={TestErrors} />
                 <Route path="/server-error" component={ServerError} />
-                <Route path="/login" component={LoginForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
